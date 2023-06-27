04693f9c87883498ca929bc23664b467d9e6f227
Russell Sim <russell.h.sim@gmail.com>
2023-06-27 15:21:52 -0400

    Adjust container size to use viewport height instead of hard coded pixel size. Add button that prompts user for grid size on click

--- javascript.js
+++ javascript.js
@@ -4,12 +4,17 @@ for (j = 0; j < 16; j++) {
     for (i = 0; i < 16; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
-        square.style.width = `calc(500px / 16)`
-        square.style.height = `calc(500px / 16)`
+        square.style.width = `calc(96.5vh / 16)`
+        square.style.height = `calc(96.5vh / 16)`
         square.addEventListener('mouseover', function (e) {
             e.target.style.background = 'blue';
         });
         row.appendChild(square);
     }
     container.appendChild(row);
+}
+
+const genButton = document.querySelector('#genButton');
+genButton.onclick = function(){
+    let gridNum = prompt("Enter the number of squares per side you'd like in your grid! (Max 100)");
 }
