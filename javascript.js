d792ad04b3ce354a7e15f355de738a0446203282
Russell Sim <russell.h.sim@gmail.com>
2023-06-27 14:53:11 -0400

    Set up CSS styles to have the squares scale properly so that the grid always fits in 500px container no matter the number of squares

--- javascript.js
+++ javascript.js
@@ -3,13 +3,13 @@ for (j = 0; j < 16; j++) {
     const row = document.createElement('div');
     for (i = 0; i < 16; i++) {
         const square = document.createElement('div');
-        square.style.width = '16px';
-        square.style.height = '16px';
-        square.style.border = 'solid 1px black';
+        square.classList.add('square');
+        square.style.width = `calc(500px / 16)`
+        square.style.height = `calc(500px / 16)`
         square.addEventListener('mouseover', function (e) {
             e.target.style.background = 'blue';
-        })
+        });
         row.appendChild(square);
-    };
+    }
     container.appendChild(row);
-};
+}
