e886f709e6c70bc12670739a687086dd91c9950b
Russell Sim <russell.h.sim@gmail.com>
2023-06-27 15:50:16 -0400

    Adjust genButton onclick function to generate new grid based on user input, clearing default 16x16 grid and replacing with new one

--- javascript.js
+++ javascript.js
@@ -1,4 +1,28 @@
 const container = document.querySelector('#container');
+const genButton = document.querySelector('#genButton');
+
+genButton.onclick = function(){
+    let gridNum = prompt("Enter the number of squares per side you'd like in your grid! (Max 100)");
+    gridNum = parseInt(gridNum);
+
+    container.innerHTML = '';
+
+    for (j = 0; j < gridNum; j++) {
+        const row = document.createElement('div');
+        for (i = 0; i < gridNum; i++) {
+            const square = document.createElement('div');
+            square.classList.add('square');
+            square.style.width = `calc(96.5vh / ${gridNum})`
+            square.style.height = `calc(96.5vh / ${gridNum})`
+            square.addEventListener('mouseover', function (e) {
+                e.target.style.background = 'blue';
+            });
+            row.appendChild(square);
+        }
+        container.appendChild(row);
+    }
+}
+
 for (j = 0; j < 16; j++) {
     const row = document.createElement('div');
     for (i = 0; i < 16; i++) {
@@ -12,9 +36,4 @@ for (j = 0; j < 16; j++) {
         row.appendChild(square);
     }
     container.appendChild(row);
-}
-
-const genButton = document.querySelector('#genButton');
-genButton.onclick = function(){
-    let gridNum = prompt("Enter the number of squares per side you'd like in your grid! (Max 100)");
 }
